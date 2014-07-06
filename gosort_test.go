0bd892abc273d2063f010b1e70524e2be98dc082
deffer <irina.benediktovich@gmail.com>
2014-07-06 20:25:18 +1200

    Quicksort broke, cant fix, giving up

--- gosort_test.go
+++ gosort_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-var initial = []int{5, 9, 12, 2, 6, -1, 200, 201, 10, -1000, 1000, 2}
+var initial = []int{5, 9, 12, 2, 6, -1, 200, 201, 10, -1000, 1000, 2, 11}
 
 type Order struct {
 	number int
@@ -15,33 +15,21 @@ func (o Order) String() string {
 	return strconv.Itoa(o.number)
 }
 
-func byNumber(one, two interface{}) int {
-	o1, o2 := one.(Order), two.(Order) // SPARTAAAAAA!!!!!!
-
-	if o1.number > o2.number {
-		return 1
-	} else if o1.number == o2.number {
-		return 0
-	} else {
-		return -1
-	}
-}
-
 func Test(t *testing.T) {
-	orders := make([]Order, 12)
+	orders := make([]Order, 13)
 
 	for i := range initial {
 		orders[i] = Order{initial[i]}
 	}
 
-	SortStructs(orders, func(i, j int) bool {
+	SortStructs(orders, func(i, j int) bool { // <-------------------
 		return orders[i].number < orders[j].number
 	})
 
 	for i := range orders {
 		if i > 0 {
-			if byNumber(orders[i-1], orders[i]) == 1 {
-				t.Errorf("Wrong sort, %v is bigger than %v", orders[i-1], orders[i])
+			if orders[i-1].number > orders[i].number {
+				t.Errorf("Wrong sort (structures), %v is bigger than %v", orders[i-1], orders[i])
 			}
 		}
 
